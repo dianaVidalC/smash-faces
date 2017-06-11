1c299672f91cc74dad5e11a5a4e8c7e565780b54
miriampc <miiriampc@gmail.com>
2017-06-10 23:26:13 -0500

    structure of play section

--- js/components/search.js
+++ js/components/search.js
@@ -1,25 +1,59 @@
 /*Created by @miriam on 9/06/2017.
  */
 "use strict";
-const coder
+const coderImage = (sede,update)=>{
+  const coderContainer = $('<div class="coder-container"></div>');
+  const coderImg = $(`<img src="" alt="">`);
 
-const SearchSede = (update)=>{
-  const searchContainer = $('<section class="search"></section>');
+
+  coderContainer.append(coderImg);
+  container.append(coderContainer);
+  return container;
+}
+
+const reRender = (coderContainer,sede, update)=>{
+  coderContainer.empty();
+
+}
+
+const Play = (update)=>{
+  const playContainer = $('<section class="play-container"></section>');
+  const selectContainer = $('<div class="select-container"></div>');
+  const container = $('<div class="container"></div>');
   const span = $('<span>Elige tu sede:</span>');
   const select = $('<select id="sede"></select>');
-  const option0 = $('<option value="0">Selecciona una opción</option>');
+  const option0 = $('<option value="0" hidden>Selecciona una opción</option>');
   const option1 = $('<option value="1">Lima</option>');
   const option2 = $('<option value="2">Mexico</option>');
   const divPoints = $('<div></div>');
-  const  points = $(`<span>Puntos: ${points} </span>`);
+  //const points = $(`<span>Puntos: ${points} </span>`);
+  const coderContainer = $('<div class="coder-container"></div>');
+  const coderSearch = $('<div class="coder-search"></div>');
+  const label = $('<label>Ingresa su nombre:</label>');
+  const input = $('<input type="text" placeholder="Nombre">');
+  const button = $('<button>COMPROBAR</button>');
 
-  searchContainer.append(span);
-  searchContainer.append(select);
-  searchContainer.append(points);
+  selectContainer.append(span);
+  select.append(option0);
+  select.append(option1);
+  select.append(option2);
+  selectContainer.append(select);
+  selectContainer.append(divPoints);
+  playContainer.append(selectContainer);
+  container.append(coderContainer);
+  coderSearch.append(label);
+  coderSearch.append(input);
+  coderSearch.append(button);
+  container.append(coderSearch);
+  playContainer.append(container);
 
-  $('#sede').selected()
 
+  select.on('change', (e) =>{
+    e.preventDefault();
+    let sede = $('select option:selected').text();
+    // reRender(coderContainer,sede,update);
+  });
+  // reRender(result,"",update);
 
-
-  return searchContainer;
+  return playContainer;
 };
--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@
 const render = (root)=>{
     root.empty();
     const wrapper = $('<div class="wrapper"></div>');
-    wrapper.append(SearchSede(_=>render(root)));
+    wrapper.append(Play(_=>render(root)));
 
     root.append(wrapper);
     //return root;
 };
 
 const state = [{
-    coders : null,
-    selectedCoder : null
+    coder : null,
+    selectedSede : null
 }];
 $( _ => {
     getJSON('coders.json',(error, json)=>{
         if (error){return alert(error.message);}
-        state.coders = json;
+        state.coder = json;
         const root = $('.root');
         render(root);
     });
