9ff066569df04d326956bd06a5755b12d4146702
miriampc <miiriampc@gmail.com>
2017-06-11 23:37:17 -0500

    change coder image with setTimeout

--- js/utils/filter.js
+++ js/utils/filter.js
@@ -12,20 +12,20 @@ const checkName = (coder,nameCoder,update) =>{
   let message = {}, refresh;
   if(coder.name == nameCoder.val()){
     message={msj:"Excelente Acertaste",point:point+5};
-    //refresh=setInterval(update,3000);
+    setTimeout(update,2000);
     nameCoder.val("");
-    //clearInterval(refresh);
+    message.msj="";
   }else {
     message={msj:"Sigue Intentando",point:point};
     nameCoder.val("");
     intentos++;
     if(intentos==5){
       message={msj:"",point:point-1};
+      setTimeout(update,2000);
       intentos=0;
     }
     console.log(intentos);
   }
   point=message.point;
-//  update();
   return message;
 }
